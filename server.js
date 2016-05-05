31e7b0a5fd4159581c87007b463759eacb85d60a
Noah Bumgardner <noah@playgamecafe.com>
2016-05-04 22:22:58 -0500

    Add comment to each section of `server.js`.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+// Import modules
 var connect = require('connect');
 var http = require('http');
 var serveStatic = require('serve-static');
 
+// Create middleware functions, from nodeJS, to set up server
 var app = connect();
 app.use(serveStatic('./dist'));
 app.use(serveStatic('./'));
 
+// Start server
 console.log('Starting webserver on http://localhost:8080/');
 http.createServer(app).listen(8080);
