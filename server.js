ba0a5789c9f4e8497bc9f15a626f5bf7745ed06f
Noah Bumgardner <noah@playgamecafe.com>
2016-05-06 13:44:09 -0500

    Refresh is the only bugged navigation. Cleaned up `server.js` comments. Used if-statement with regex of views; it works. Included too many comments.

--- server.js
+++ server.js
@@ -10,17 +10,25 @@ var app = connect();
 app.use(serveStatic('./dist'));
 app.use(serveStatic('./'));
 
+// TODO: Debug refresh going to wrong view. Problem may lie elsewhere.
 // Map all the expected SPA (Single Page App.) URLs to the root index.html
 app.use(function (req, res, next) {
   var reqUri = url.parse(req.url);
-  // Brainstorming correct regex to cover all valid URLs in the SPA.
-  // var regex = /^\/[0-9]+/
-  // var regex_views = /^\/(choose-video|watch-video|save-notes)$/
-  // var regex_views_with_search = /^\/(choose-video(\/\w+)*|watch-video|save-notes)$/
-  // var regex_youtube = /^http://(?:www\.)?youtu(?:be\.com/watch\?v=|\.be/)([\w\-]+)(&(amp;)?[\w\?=]*)?$/
-  // regex = regex;
-  // if (regex.test(requestURI.pathname)) {
-  if (/^\/[0-9]+/.test(reqUri.pathname)) {
+  /*
+  // Brainstorm correct regex to cover all valid URLs in the SPA.
+  var regex = /^\/[0-9]+/
+  var regex_views = /^\/(choose-video|watch-video|save-notes)$/
+  var regex_views_with_search = /^\/(choose-video(\/\w+)*|watch-video|save-notes)$/
+  var regex_youtube = /^http://(?:www\.)?youtu(?:be\.com/watch\?v=|\.be/)([\w\-]+)(&(amp;)?[\w\?=]*)?$/
+  regex = regex;
+  */
+  // regex recognizes URLs of the three views.
+  //   Refresh does not give error when URL is recognized.
+  //   Not guaranteed to show correct view when refreshed.
+  var regex = /^\/(choose-video|watch-video|save-notes)$/;
+
+  //if (true) { // To skip test: Comment-in this, comment-out below line.
+  if (regex.test(reqUri.pathname)) { // Good regex. Bug occurs on refresh.
     fs.readFile('./dist/index.html', { encoding: 'utf8' }, function (err, data) {
       if (err) {
         throw err;
